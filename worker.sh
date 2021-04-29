cc55c96cb9c578a6c85ff6453fae1ff2100e2da7
Carlos Alvarado <carlos@asustuf.attlocal.net>
2021-04-29 18:23:32 -0400

    Environment variable

--- worker.sh
+++ worker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
+export PX_METADATA_NODE_LABEL="kubernetes.io/role=infra"
+
 kubeadm join master.example.com:6443 --token ${TOKEN} \
   --discovery-token-unsafe-skip-ca-verification
